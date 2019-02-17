84ee270e4c6a8eee17303a85df5075273bc9379f
saravananjagan <43237439+saravananjagan@users.noreply.github.com>
2019-02-18 00:09:20 +0530

    Initial Commit Authentication and Authorization

---
+++ AuthenticationLearning_WithoutWebApi/AuthenticationLearning_WithoutWebApi/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using AuthenticationLearning_WithoutWebApi.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PMSProxy.FetchMetadata;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationLearning_WithoutWebApi.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [Authorize]
+        public ActionResult Index()
+        {
+            string TenantId=TenantDetailsProxy.FetchTenantId();
+            if (User.Identity.IsAuthenticated)
+            {                
+                
+                //DataSet ModuleDisplayDetails=ModuleDisplayDetailsProxy.FetchModuleDisplayDetails(TenantId,User.Identity.)
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View();
+        }
+
+        private Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {                
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ AuthenticationLearning_WithoutWebApi/PMSDAL/FetchMetadata/ModuleDisplayDetailsDAL.cs
@@ -0,0 +1,36 @@
+﻿using PMSIDAL.FetchMetadata;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSDAL.FetchMetadata
+{
+    public class ModuleDisplayDetailsDAL : IModuleDisplayDetailsDAL
+    {
+        private string ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public DataSet FetchModuleDisplayDetails(string TenantId, string RoleId)
+        {
+            DataSet ModuleDisplayDetails = new DataSet();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand("dbo.USP_FetchModuleDisplayDetails",connection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add("@TenantId",SqlDbType.UniqueIdentifier).Value=Guid.Parse(TenantId);
+                    sqlCommand.Parameters.Add("@RoleId", SqlDbType.UniqueIdentifier).Value = Guid.Parse(RoleId);
+                }
+                    return ModuleDisplayDetails;
+            }
+            finally
+            {
+                ModuleDisplayDetails = null;
+            }
+        }
+    }
+}
